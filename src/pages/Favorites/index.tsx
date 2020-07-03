cd376024febdf4f5a1900a0f37ff9813449f149c
Cesar Menegatti <cesarmenegatti@Cesars-MacBook-Pro.local>
2020-07-03 15:36:25 -0300

    ✨Challenge Done!!

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -32,7 +32,14 @@ const Favorites: React.FC = () => {
 
   useEffect(() => {
     async function loadFavorites(): Promise<void> {
-      // Load favorite foods from api
+      const getFavorites = await api.get('favorites');
+
+      setFavorites(
+        getFavorites.data.map((favorite: Food) => ({
+          ...favorite,
+          formattedPrice: formatValue(favorite.price),
+        })),
+      );
     }
 
     loadFavorites();
